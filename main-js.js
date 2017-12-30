4b9d766d1d7b6da26b63383f3b70d7bb9d14c61d
Rycintech <rycintech@gmail.com>
2017-12-30 03:28:56 -0800

    Added contact info

---
+++ main-js.js
@@ -0,0 +1,16 @@
+(function($) {
+    $.fn.goTo = function() {
+        $('html, body').animate({
+            scrollTop: $(this).offset().top + 'px'
+        }, 1000);
+        return this;
+    }
+})(jQuery);
+
+$("#drop-down-button-1").click(function(){
+  $("#container-2").goTo();
+});
+
+$("#drop-down-button-2").click(function(){
+  $("#container-3").goTo();
+})
